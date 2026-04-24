f4668b8258f563642fc1414d67cd1c0b3d5c922a
GAUSIC R M <gausic.rm28@gmail.com>
2026-04-24 11:47:56 +0530

    Time: 25 ms (12.84%) | Memory: 76.3 MB (99.14%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,11 @@
-import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
-        for (int num : nums) {
-            if (seen.contains(num)) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
             }
-            seen.add(num);
-        } 
+        }
         return false;
     }
 }

1ffb81596b306d9ee338d2cdf81c2206a0e2e478
GAUSIC R M <gausic.rm28@gmail.com>
2026-05-17 14:49:56 +0530

    Time: 130 ms (73.94%) | Memory: 42.7 MB (87.35%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -11,20 +11,22 @@ class Solution {
         }
         return false;
     }
-    private boolean dfs(char[][] board, String word, int i, int j, int index) {
-        if (index == word.length()) return true;
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length 
-            || board[i][j] != word.charAt(index)) {
+    public boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length
+                || board[i][j] != word.charAt(index)) {
             return false;
         }
         char temp = board[i][j];
         board[i][j] = '#';
-        boolean found = dfs(board, word, i + 1, j, index + 1) ||
-                        dfs(board, word, i - 1, j, index + 1) ||
-                        dfs(board, word, i, j + 1, index + 1) ||
-                        dfs(board, word, i, j - 1, index + 1);
+        boolean found =
+                dfs(board, word, i + 1, j, index + 1) ||
+                dfs(board, word, i - 1, j, index + 1) ||
+                dfs(board, word, i, j + 1, index + 1) ||
+                dfs(board, word, i, j - 1, index + 1);
         board[i][j] = temp;
-
         return found;
     }
 }

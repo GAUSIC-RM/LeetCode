b6d358c9bd37a18e30f0e75f628ade25540b8712
GAUSIC R M <gausic.rm28@gmail.com>
2026-05-17 14:51:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (9.27%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,31 +1,29 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] matrix = new int[n][n];
-        int top = 0, bottom = n - 1;
-        int left = 0, right = n - 1;
+        int[][] ans = new int[n][n];
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = n - 1;
         int num = 1;
         while (top <= bottom && left <= right) {
-            for (int col = left; col <= right; col++) {
-                matrix[top][col] = num++;
+            for (int i = left; i <= right; i++) {
+                ans[top][i] = num++;
             }
             top++;
-            for (int row = top; row <= bottom; row++) {
-                matrix[row][right] = num++;
+            for (int i = top; i <= bottom; i++) {
+                ans[i][right] = num++;
             }
             right--;
-            if (top <= bottom) {
-                for (int col = right; col >= left; col--) {
-                    matrix[bottom][col] = num++;
-                }
-                bottom--;
+            for (int i = right; i >= left; i--) {
+                ans[bottom][i] = num++;
             }
-            if (left <= right) {
-                for (int row = bottom; row >= top; row--) {
-                    matrix[row][left] = num++;
-                }
-                left++;
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                ans[i][left] = num++;
             }
+            left++;
         }
-        return matrix;
+        return ans;
     }
 }

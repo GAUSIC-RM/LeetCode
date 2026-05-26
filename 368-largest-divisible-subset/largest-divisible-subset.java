1f80e0ad7ccb08833bba8b40ae034ae9e6c0d202
GAUSIC R M <gausic.rm28@gmail.com>
2026-05-26 23:10:02 +0530

    Time: 20 ms (19.00%) | Memory: 47.5 MB (8.10%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<Integer>[] dp = new ArrayList[n];
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            dp[i] = new ArrayList<>();
+            dp[i].add(nums[i]);
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 &&
+                    dp[j].size() + 1 > dp[i].size()) {
+                    dp[i] = new ArrayList<>(dp[j]);
+                    dp[i].add(nums[i]);
+                }
+            }
+            if (dp[i].size() > answer.size()) {
+                answer = dp[i];
+            }
+        }
+        return answer;
+    }
+}
